267572db49dc4106b09dc2573a689236b878205c
EdwinSemakula <edwin.semakula12@btinternet.com>
2023-07-28 18:55:27 +0100

    Initial commit

---
+++ src/main/java/com/java/SkincareRoutineBuilder/SkincareRoutineBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.java.SkincareRoutineBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareRoutineBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareRoutineBuilderApplication.class, args);
+	}
+
+}
